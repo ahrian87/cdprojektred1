5265e93ecc602ccd9bd4064dc3cebab98ed96ba9
ahrian87 <maciej.zbydniewski@gmail.com>
2022-10-31 14:40:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("Pchaj Dywan", "kleryk", 7, 120, true);
+        System.out.println(hero1.power(7, 120));
+
+    }
+}
